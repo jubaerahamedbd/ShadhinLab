6a3d8db8df3bb57828263bbfb52965a0b78c4a56
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-02 23:23:36 +0600

    Modified Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,80 +6,72 @@ import AddUser from "./Users/AddUser";
 const Home = () => {
     const [tabValue, settabValue] = useState('admin')
     const [users, setUsers] = useState([])
-
     const [openModal, setOpenModal] = useState(false)
+
     useEffect(() => {
-        
+        setUsers([])
         loadUsers()
     }, [tabValue])
 
     const loadUsers = async () => {
-        const result = await axios.get("https://60f2479f6d44f300177885e6.mockapi.io/users")
+        const result = await axios.get(
+            `https://60f2479f6d44f300177885e6.mockapi.io/users?user_type=${tabValue}&?page=${0}&limit=${5}`
+        );
         result.data.reverse()
-        setUsers(
-            result.data.filter((data) => {
-                if (data.user_type === tabValue) return true;
-            })
-        )
-    }
-    const deleteUser = async id => {
-        await axios.delete(`https://60f2479f6d44f300177885e6.mockapi.io/users/${id}`)
+        setUsers(result.data);
+    };
+    const deleteUser = async (id) => {
+        await axios.delete(`https://60f2479f6d44f300177885e6.mockapi.io/users/${id}`);
         loadUsers();
-    }
-   
+    };
+
     return (
         <div className="home-container">
-        
-        <div className="container">
-        {openModal && <AddUser closeModal={setOpenModal} setUsers={setUsers} tabValue={tabValue} />}
-            <div className="top">
-                <h3 className="page-title">Users List</h3>
-                <Link className="back-button-home" id="addUser" onClick={() => setOpenModal(true)} to="/">Add User</Link>
-            </div>
-            <hr />
+            <div className="container">
+                {openModal && (<AddUser closeModal={setOpenModal} settabValue={settabValue} />)}
+                <div className="top">
+                    <h3 className="page-title">Users List</h3>
+                    <Link className="back-button-home" id="addUser" onClick={() => setOpenModal(true)} to="/">Add User</Link>
+                </div>
+                <hr />
 
-            <div className={openModal === false? "tabshow":"tabhide"}>
-                <button className={tabValue ==="admin" ? "admin":"button"} id="admin" name="mytabs" checked="checked" onClick={() => settabValue('admin')}>Admin</button>
-                <button className={tabValue ==="employee" ? "employee":"button"} id="employee" name="mytabs" checked="checked" onClick={() => settabValue('employee')}>Employee</button>
-              
-                <div className="tab">
-                    <table class="table">
-                        <thead>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Division</th>
-                            <th scope="col">District</th>
-                            <th scope="col">User Type</th>
-                            <th scope="col">Details View</th>
+                <div className={openModal === false ? "tabshow" : "tabhide"}>
+                    <button className={tabValue === "admin" ? "admin" : "button"} id="admin" name="mytabs" checked="checked" onClick={() => settabValue('admin')}>Admin</button>
+                    <button className={tabValue === "employee" ? "employee" : "button"} id="employee" name="mytabs" checked="checked" onClick={() => settabValue('employee')}>Employee</button>
 
-                        </thead>
-                        <tbody>
-                            {
+                    <div className="tab">
+                        <table class="table">
+                            <thead>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Division</th>
+                                <th scope="col">District</th>
+                                <th scope="col">User Type</th>
+                                <th scope="col">Details View</th>
 
-                                users.map((user) => (
-
+                            </thead>
+                            <tbody>
+                                {users && users.map((user) => (
                                     <tr>
                                         <td data-label="First Name">{user.first_name}</td>
                                         <td data-label="Last Name">{user.last_name} </td>
                                         <td data-label="Division">{user.division}</td>
                                         <td data-label="District">{user.district}</td>
                                         <td data-label="User Type">{user.user_type}</td>
-                                        <td data-label="Details View"><Link className="btn-primary" to={`/user/${user.id}`}>Details</Link>
-                                            <Link className="btn-warn" onClick={() => deleteUser(user.id)}>Delete</Link>
+                                        <td data-label="Details View">
+                                            <Link className="btn-primary" to={`/user/${user.id}`}>Details </Link>
+                                            <Link className="btn-warn" onClick={() => deleteUser(user.id)} to="/"> Delete</Link>
                                         </td>
-
                                     </tr>
+                                ))}
 
-                                ))
-                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
 
-                        </tbody>
-                    </table>
-                </div>                          
+
             </div>
-            
-            
-        </div>
         </div>
     )
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Country, State, City } from "country-state-city";
-const AddUser = ({ closeModal, setUsers, tabValue }) => {
+import { State, City } from "country-state-city";
+import { Formik } from "formik";
 
-    const [user, setUser] = useState({
+const AddUser = ({ closeModal, settabValue }) => {
+    const [users, setUsers] = useState({
         first_name: "",
         last_name: "",
         user_type: "",
         division: "",
-        district: ""
-    })
-
-    const { first_name, last_name, user_type, division, district } = user
+        district: "",
+    });
     const [divisions, setDivisions] = useState(
         State.getStatesOfCountry("BD").filter((dataItem) => {
             if (dataItem.name.includes("Division")) return true;
@@ -22,154 +20,202 @@ const AddUser = ({ closeModal, setUsers, tabValue }) => {
     );
     const [districts, setDistricts] = useState([]);
 
-    const OnInputChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]:
-                e.target.name === "division"
-                    ? e.target.value.split("-")[0]
-                    : e.target.value,
-        });
-        if (e.target.user_type === 'admin') {
-            tabValue = 'admin'
+    const postUserData = async (useData) => {
+        await axios.post("https://60f2479f6d44f300177885e6.mockapi.io/users", useData);
+        const result = await axios.get(`https://60f2479f6d44f300177885e6.mockapi.io/users?user_type=${useData.user_type}&?page=${0}&limit=${5}`);
+        result.data.reverse();
+        if (useData.user_type === "admin") {
+            settabValue("admin");
+        } else {
+            settabValue("employee");
+        }
+        setUsers(result.data);
+        closeModal(false);
+    };
+    const initialValues = {
+        first_name: "",
+        last_name: "",
+        user_type: "admin",
+        division: "",
+        district: "",
+    };
+    const validate = (values) => {
+
+        let errors = {};
+        if (!values.first_name) {
+            errors.first_name = "first name is required";
+        }
+        if (!values.last_name) {
+            errors.last_name = "last name is required";
+        }
+        if (values.user_type === "employee" && !values.division) {
+            errors.division = "Division is required";
+        }
+        if (values.user_type === "employee" && !values.district) {
+            errors.district = "District is required";
         }
 
+        if (!values.user_type) {
+            errors.user_type = "user type is required";
+        }
+        return errors;
     };
-    const onSubmit = async e => {
-        e.preventDefault()
-        await axios.post("https://60f2479f6d44f300177885e6.mockapi.io/users", user)
-        const result = await axios.get("https://60f2479f6d44f300177885e6.mockapi.io/users")
-        result.data.reverse()
-        setUsers(
-            result.data.filter((data) => {
-                if (data.user_type === tabValue) return true;
-            })
-        )
-        closeModal(false)
-    }
-    useEffect(() => {
-
-    }, []);
-    let area;
-    if (user_type === "employee") {
-        area = (<div>
-            <div class="form-group row my-2">
-                <label class="col-sm-3 col-form-label col-form-label-sm">
-                    Division
-                </label>
-                <div class="col-sm-9">
-                    <select
-                        onChange={(e) => {
-                            OnInputChange(e);
-                            setDistricts(
-                                City.getCitiesOfState("BD", e.target.value.split("-")[1])
-                            );
-                        }}
-                        class="form-control form-control-sm"
-                        name="division"
-                    >
-                        {divisions.map((dataItem) => {
-                            return (
-                                <option value={dataItem.name + "-" + dataItem.isoCode}>
-                                    {dataItem.name}
-                                </option>
-                            );
-                        })}
-                    </select>
-                </div>
-            </div>
-            <div class="form-group row my-2">
-                <label class="col-sm-3 col-form-label col-form-label-sm">
-                    District
-                </label>
-                <div class="col-sm-9">
-                    <select
-                        onChange={(e) => {
-                            OnInputChange(e);
-                        }}
-                        class="form-control form-control-sm"
-                        name="district"
-                    >
-                        {districts.map((dataItem) => {
-                            return <option value={dataItem.name}>{dataItem.name}</option>;
-                        })}
-                    </select>
-                </div>
-            </div>
-        </div>
-        )
-    } else {
-        area = ''
-    }
-
+    const submitForm = async (values) => {
+        let userData = {
+            ...values,
+            ...{ division: values.division.split("-")[0] },
+        };
+        postUserData(userData);
+    };
     return (
-        <div className="AddUserLayout">
-            <div className="container">
-                <div className="col-md-8 updateUser">
-                    <div className="top">
-                        <h2 className="page-title">Add User</h2>
-                        <Link className="back-button" id="addUser" onClick={() => closeModal(false)} to="/">Back To User List</Link>
+        <Formik
+            initialValues={initialValues}
+            validate={validate}
+            onSubmit={submitForm}
+        >
+            {(formik) => {
+                const {
+                    values,
+                    handleChange,
+                    handleSubmit,
+                    errors,
+                    touched,
+                    handleBlur,
+                    isValid,
+                    dirty,
+                } = formik;
+                return (
+                    <div className="AddUserLayout">
+                        <div className="container">
+                            <div className="col-md-8 updateUser">
+                                <div className="top">
+                                    <h2 className="page-title">Add User</h2>
+                                    <Link className="back-button" id="addUser" onClick={() => closeModal(false)} to="/" >Back To User List</Link>
+                                </div>
+                                <hr />
+                                <form onSubmit={handleSubmit}>
+                                    <div class="form-group row my-2">
+                                        <label class="col-sm-3 col-form-label col-form-label-sm">First Name:</label>
+                                        <div class="col-sm-9">
+                                            <input
+                                                class="col-sm-9"
+                                                type="text"
+                                                class="form-control"
+                                                name="first_name"
+                                                value={values.first_name}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                placeholder="Enter First Name"
+                                            />
+                                            {errors.first_name && touched.first_name && (<span className="error">{errors.first_name}</span>)}
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row my-2">
+                                        <label class="col-sm-3 col-form-label col-form-label-sm">Last Name</label>
+                                        <div class="col-sm-9">
+                                            <input
+                                                type="text"
+                                                class="form-control"
+                                                name="last_name"
+                                                value={values.last_name}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                placeholder="Enter Last Name"
+                                            />
+                                            {errors.last_name && touched.last_name && (<span className="error">{errors.last_name}</span>)}
+                                        </div>
+                                    </div>
+
+                                    {values.user_type === "employee" ? (
+                                        <div>
+                                            <div class="form-group row my-2">
+                                                <label class="col-sm-3 col-form-label col-form-label-sm">
+                                                    Division
+                                                </label>
+                                                <div class="col-sm-9">
+                                                    <select
+                                                        onChange={(e) => {
+                                                            handleChange(e);
+                                                            setDistricts(
+                                                                City.getCitiesOfState(
+                                                                    "BD",
+                                                                    e.target.value.split("-")[1]
+                                                                )
+                                                            );
+                                                        }}
+                                                        value={values.division}
+                                                        onBlur={handleBlur}
+                                                        class="form-control form-control-sm"
+                                                        name="division"
+                                                    >
+                                                        <option disabled selected value={""}>-- select a division --</option>
+                                                        {divisions.map((dataItem) => {
+                                                            return (
+                                                                <option value={dataItem.name + "-" + dataItem.isoCode} >
+                                                                    {dataItem.name}
+                                                                </option>
+                                                            );
+                                                        })}
+                                                    </select>
+                                                    {errors.division && touched.division && (<span className="error">{errors.division}</span>)}
+                                                </div>
+                                            </div>
+                                            <div class="form-group row my-2">
+                                                <label class="col-sm-3 col-form-label col-form-label-sm">District</label>
+                                                <div class="col-sm-9">
+                                                    <select
+                                                        onChange={handleChange}
+                                                        value={values.district}
+                                                        onBlur={handleBlur}
+                                                        class="form-control form-control-sm"
+                                                        name="district"
+                                                    >
+                                                        <option disabled value={""}>-- select a district --</option>
+                                                        {districts.map((dataItem) => {
+                                                            return (
+                                                                <option value={dataItem.name}>
+                                                                    {dataItem.name}
+                                                                </option>
+                                                            );
+                                                        })}
+                                                    </select>
+                                                    {errors.district && touched.district && (<span className="error">{errors.district}</span>)}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <></>
+                                    )}
+
+                                    <div class="form-group row my-2">
+                                        <label class="col-sm-3 col-form-label col-form-label-sm">User Type</label>
+                                        <div class="col-sm-9">
+                                            <select
+                                                value={values.user_type}
+                                                onChange={(e) => {
+                                                    handleChange(e);
+                                                }}
+                                                onBlur={handleBlur}
+                                                class="form-control form-control-sm"
+                                                name="user_type"
+                                            >
+                                                <option value={"admin"}>admin</option>
+                                                <option value={"employee"}>employee</option>
+                                            </select>
+                                            {errors.user_type && touched.user_type && (<span className="error">{errors.user_type}</span>)}
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary updatebtn"> Add User</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <hr />
-                    <form onSubmit={e => onSubmit(e)}>
-                        <div class="form-group row my-2">
-
-                            <label class="col-sm-3 col-form-label col-form-label-sm">First Name:</label>
-
-                            <div class="col-sm-9" >
-                                <input
-                                    class="col-sm-9"
-                                    type="text"
-                                    class="form-control"
-                                    name="first_name"
-                                    value={first_name}
-                                    placeholder="Enter First Name"
-                                    onChange={e => OnInputChange(e)}
-                                />
-                            </div>
-                        </div>
-
-                        <div class="form-group row my-2">
-
-                            <label class="col-sm-3 col-form-label col-form-label-sm">Last Name</label>
-                            <div class="col-sm-9" >
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    name="last_name"
-                                    value={last_name}
-                                    placeholder="Enter Last Name"
-                                    onChange={e => OnInputChange(e)}
-                                />
-                            </div>
-
-                        </div>
-
-                        {area}
-
-                        <div class="form-group row my-2">
-                            <label class="col-sm-3 col-form-label col-form-label-sm"    >User Type</label>
-                            <div class="col-sm-9">
-
-                                <select
-                                    onChange={(e) => OnInputChange(e)}
-                                    class="form-control form-control-sm"
-                                    name="user_type"
-                                >
-                                    <option value={"admin"}>admin</option>
-                                    <option value={"employee"}>employee</option>
-                                </select>
-                            </div>
-
-                        </div>
-
-                        <button type="submit" class="btn btn-primary updatebtn">Add User</button>
-                    </form>
-                </div>
-
-            </div>
-        </div>
-    )
-}
+                );
+            }}
+        </Formik>
+    );
+};
 
 export default AddUser;
